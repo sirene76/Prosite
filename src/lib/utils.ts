c0bad73de69da96b9b99a59dd3c439e9f1218ab1
benjouadfatimaezzahra-maker <benjouadfatimaezzahra@example.com>
2025-10-10 00:36:44 +0200

    add tools

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,6 @@
-import { randomBytes } from "crypto";
-import clsx, { type ClassValue } from "clsx";
-
-export function generatePreviewToken() {
-  return randomBytes(16).toString("hex");
-}
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return clsx(...inputs);
+  return twMerge(clsx(inputs))
 }
