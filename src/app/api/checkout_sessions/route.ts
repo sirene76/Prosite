3b3cd34a36f394c8f90a24084b61499b4f8d7d3b
benjouadfatimaezzahra-maker <benjouadfatimaezzahra@example.com>
2025-10-21 02:05:51 +0200

    fix prob with route

--- src/app/api/checkout_sessions/route.ts
+++ src/app/api/checkout_sessions/route.ts
@@ -7,8 +7,9 @@ import { connectDB } from "@/lib/mongodb";
 import Website from "@/models/Website";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2023-10-16",
 });
+
 
 export async function POST(req: Request) {
   try {
