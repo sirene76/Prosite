9ca6681c06f6d9b70fc4ff1727213bebe96d236d
sirene76 <saidcours04@gmail.com>
2025-10-31 01:41:21 +0100

    Refine builder layout and styling

--- src/app/builder/[websiteId]/layout.tsx
+++ src/app/builder/[websiteId]/layout.tsx
@@ -1,18 +1,10 @@
-import type { ReactNode } from "react";
 import "../../../styles/builder-dark.css";
 import BuilderShell from "./BuilderShell";
-
-interface BuilderLayoutProps {
-  children: ReactNode;
-  params: Promise<{
-    websiteId: string;
-  }>;
-}
 
 export default async function BuilderLayout({
   children,
   params,
-}: BuilderLayoutProps) {
+}: any) {
   const { websiteId } = await params;
 
   return <BuilderShell websiteId={websiteId}>{children}</BuilderShell>;
